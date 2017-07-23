f910e27519b32d09dcd19f70d2fd1ee6c6736901
Kousuke Nagatsuka <koron393@gmail.com>
2017-07-24 00:36:32 +0900

    z2_plus: Pixel bootanimation
    *https://forum.xda-developers.com/lenovo-zuk-z2/themes/google-pixel-bootanimation-black-t3575064

--- device.mk
+++ device.mk
@@ -307,3 +307,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
+
+# BootAnimation
+PRODUCT_BOOTANIMATION := $(LOCAL_PATH)/bootanimation.zip
